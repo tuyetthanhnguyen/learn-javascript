2136e4565b1b544a5cf83c01f7d258aec6043a4b
tuyetthanhnguyen2001lqd@gmail.com <tuyetthanhnguyen2001lqd@gmail.com>
2023-01-06 20:50:20 +0700

    ham

---
+++ 3.1_Function_JSBasic.js
@@ -0,0 +1,6 @@
+var x = 3
+var y = 6
+function tong() {
+    x += y
+    console.log(x)
+}
--- 3_codeCB.js
+++ 3_codeCB.js
@@ -1,14 +1,33 @@
-	// var a
-	// var b
-	// var c
-	// console.log("a is " + a + ", b is " + b + " and c is " + c);
-
 /*
-	function run() {
-	 a = 15;
-	 b = "javascript";
-	 c = true;
-	 console.log("a is " + a + ", b is " + b + " and c is " + c);
-}
-run()
+var a = 1
+var b = 2
+var c = a + b
+var d = a ** b
+var e = a / b
+var f = a - b
+var g = a * b
+a++
+b--
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(a)
+console.log(b)
+
 */
+var x = 2;
+var y = 3;
+
+x = y
+console.log(x)
+x += y
+console.log(x)
+x -= y
+console.log(x)
+x *= y
+x /= y
+console.log(x)
+x **= y
+console.log(x)
