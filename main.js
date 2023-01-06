4fbf21a239bdd98fe5775289dc4e8f9f36ffecad
tuyetthanhnguyen2001lqd@gmail.com <tuyetthanhnguyen2001lqd@gmail.com>
2023-01-06 14:22:57 +0700

    thong bao

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+hàm built-in
+1. Alert 
+2. Console
+3. Confirm
+4. Promt
+5. Set timeout
+6. Set interval
+*/
+// var a = 'Hello JS basic'
+// var b = '22'
+// // Alert('Hello JS')
+// console.log(a)
+// Console.log("Hello JS basic")
+// Confirm('Xac nhan')
+// Promt('Xac nhan ban cos du tuoi')
+
+// setTimeout(function () {
+//     Alert('Hello JS') 
+// }, 1000)
+
+// setInterval(function () {
+//     Alert('Hello JS') 
+// }, 1000)
+
+setInterval(function () {
+    Console.log("Hello JS basic" + Math.ra)
+}, 1000)
