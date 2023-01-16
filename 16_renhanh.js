ec7227c2f005e327bc3448c50fee21a825817fb3
tuyetthanhnguyen2001lqd@gmail.com <tuyetthanhnguyen2001lqd@gmail.com>
2023-01-16 11:31:08 +0700

    add

--- 16_renhanh.js
+++ 16_renhanh.js
@@ -15,5 +15,40 @@ console.log(run(5)) // 2
 console.log(run(15)) // 3
 
 */
+/*
+// switch case:
+var date = 6;
+switch(date) {
+    case 2:
+        console.log('Monday')
+        break;
+    case 3:
+        console.log('Tuesday')
+        break;
+    case 4:
+        console.log('Wednesday')
+        break;
+    case 5:
+        console.log('Thursday')
+        break;
+    default:
+        console.log('I do not known')
+}
 
-// 
+*/
+
+// toán tử 3 ngôi
+
+var course = {
+    name: 'Javascript',
+    coin: 250
+}
+// if (course.coin > 0){
+//     console.log(`${course.coin} Coins`)
+// }
+// else{
+//     console.log('Free')
+    
+// }
+var result = course.coin > 0 ? `${course.coin} Coins` : 'Free';
+console.log(result)
